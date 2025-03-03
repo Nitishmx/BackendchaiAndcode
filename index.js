6867859cf0fdc717e31d214ca42d6b1bd939e531
NITISH <145883497+Nitishmx@users.noreply.github.com>
2025-03-04 01:19:48 +0530

    all file setup

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Welcome to backend");
+const express=require("express")
+const app=express()
+require("dotenv").config()
+const port=process.env.PORT
+app.get("/",(req,res)=>{
+    res.send("Welcome to backend series")
+})
+app.listen(port,()=>console.log("Server is runing port no :",port))
